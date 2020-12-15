0bf15e3e09d86a6cc7fa132ceada31794507e015
emil.saracyn <emil.saracyn@gmail.com>
2020-12-15 20:07:15 +0100

    Implemented Get and Post method in InformationController

--- WirtualnyDziekanat.WebUI/Api/InformationsController.cs
+++ WirtualnyDziekanat.WebUI/Api/InformationsController.cs
@@ -8,9 +8,31 @@ using WirtualnyDziekanat.Infrastructure.Services;
 
 namespace WirtualnyDziekanat.WebUI.Api
 {
-    
+    [Route("[controller]")]
     public class InformationsController : Controller
     {
-        
+        private readonly IInformationService _informationService;
+
+        public InformationsController(IInformationService informationService)
+        {
+            _informationService = informationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string name)
+        {
+            var informations = await _informationService.BrowseAsync(name);
+
+            return Json(informations);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateInformation command)
+        {
+            command.InfoId = Guid.NewGuid();
+            await _informationService.CreateAsync(command.InfoId, command.Title, command.Contents, command.IsActive, command.PriorityStatus);
+
+            return Created($"/informations/{command.InfoId}", null);
+        }
     }
 }
