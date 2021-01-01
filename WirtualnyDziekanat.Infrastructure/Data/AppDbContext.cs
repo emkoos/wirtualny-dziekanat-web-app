f26acf08c4050b981b63cd5156d0d1e0e5c7a081
emil.saracyn <emil.saracyn@gmail.com>
2021-01-01 17:33:49 +0100

    Added Identity and migration

--- WirtualnyDziekanat.Infrastructure/Data/AppDbContext.cs
+++ WirtualnyDziekanat.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,7 @@ using WirtualnyDziekanat.Domain.Entities;
 
 namespace WirtualnyDziekanat.Infrastructure.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext<User>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
@@ -22,6 +23,8 @@ namespace WirtualnyDziekanat.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<TeacherSubject>()
                 .HasKey(ts => new { ts.TeacherId, ts.SubjectId });
            
